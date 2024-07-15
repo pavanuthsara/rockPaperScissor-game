32e8716fbbf51599c58ff84ab8231cbf110951e4
Pavan Uthsara <uthsarapavan@gmail.com>
2024-07-15 09:30:46 +0530

    generate computer choice

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ const computerChoice = document.getElementById("computerChoice");
 const possibleChoices = document.querySelectorAll('button'); //select all the choices from button tag
 
 let userChoice;
+
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener(('click'), (e) => {
     userChoice = e.target.id;
     yourChoice.innerHTML = userChoice;
+    generateComputerChoice();
 }))
+
+let randomComputerChoice;
+
+function generateComputerChoice(){
+    const randomNumber = Math.ceil(Math.random() * possibleChoices.length); //select random integer between 1-3
+    if(randomNumber == 1)
+        computerChoice.innerHTML = "rock👊";
+    else if(randomNumber == 2)
+        computerChoice.innerHTML = "paper✋";
+    else
+        computerChoice.innerHTML = "scissor✌️";
+}
+
+// function generateResult(){
+//     if(userChoice == )
+// }
 
